328ad6040ae36276b18b12c6062e0c360a9e41d1
anish26jangra <121502728+anish26jangra@users.noreply.github.com>
2025-05-07 16:32:02 +0530

    Update razorpay.js

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -2,6 +2,6 @@ const Razorpay = require("razorpay");
 require("dotenv").config();
 
 exports.instance = new Razorpay({
-	key_id: "rzp_test_FbBkoxiXuTWVYi",
-	key_secret: "gdJjV8yF4kC1ouvDdZd0TKi3",
+	key_id: process.env.RAZORPAY_KEY,
+	key_secret: process.env.RAZORPAY-SECRET,
 });
